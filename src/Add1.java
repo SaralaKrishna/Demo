28a3a049846eb73ccdece461842e0458095a76c1
SaralaGarad <garadsarala99@gmail.com>
2020-03-06 13:16:21 -0800

    This is my 1st commit for Gang

---
+++ src/Add1.java
@@ -0,0 +1,13 @@
+
+
+public class Add1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("the sum of "+a+" & "+b+ " is " +c);
+
+	}
+
+}
